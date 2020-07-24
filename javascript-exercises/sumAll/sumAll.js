133b0519bcadd323a6db213b1d8e3e5c50443c9a
mugugoo <dafifth@hotmail.fr>
2020-07-25 00:05:29 +0200

    isNan or typeof, annyoing conundrum

--- javascript-exercises/sumAll/sumAll.js
+++ javascript-exercises/sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let _ispositive, _isanumber;_sum=0;
+    _notpositive=(num1<0 || num2<0);
+    _notanumber=!(typeof num2 ==="number" || typeof num2==='number');
+    if (_notpositive || _notanumber) return  'ERROR';
 
+for (i=Math.min(num1,num2);i<Math.max(num1,num2)+1;i++)
+_sum+=i;
+
+return _sum;
 }
 
 module.exports = sumAll
