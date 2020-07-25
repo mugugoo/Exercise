04c1c939b5d2a3e3b9c39c4cefae102769146a0a
mugugoo <dafifth@hotmail.fr>
2020-07-25 15:27:40 +0200

    went crazy with string for round up. realized better to work with numbers, since numbers have Math

--- javascript-exercises/tempConversion/tempConversion.js
+++ javascript-exercises/tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+function truncating(number,decimals) {
+  _tempNum=+number*Math.pow(10,decimals);
+  _tempNum=Math.round(_tempNum);
+  return _tempNum/Math.pow(10,decimals);
 }
 
-const ctof = function() {
 
+const ftoc = function(temp) {
+
+return truncating((temp-32)*5/9,1);
+}
+
+const ctof = function(temp) {
+return truncating((temp*9/5+32),1);
 }
 
 module.exports = {
